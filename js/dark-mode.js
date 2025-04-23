0a4948c39450f6cf8ebd965eaf6ed914deb27f2b
eduardo <eduardobtez@gmail.com>
2025-04-23 20:31:19 -0300

    ajuste de modo oscuro completado

--- js/dark-mode.js
+++ js/dark-mode.js
@@ -1,11 +1,11 @@
 document.addEventListener("DOMContentLoaded", function () {
   const botonModo = document.getElementById("modoOscuroBtn");
-  const cuerpo = document.body;
+  const body = document.body;
 
   botonModo.addEventListener("click", function () {
-    cuerpo.classList.toggle("modo-oscuro");
+    body.classList.toggle("modo-oscuro");
 
-    if (cuerpo.classList.contains("modo-oscuro")) {
+    if (body.classList.contains("modo-oscuro")) {
       botonModo.textContent = "☀️ Modo Claro";
     } else {
       botonModo.textContent = "🌙 Modo Oscuro";
